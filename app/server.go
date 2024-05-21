91cd0b837a23d1481b06644193bd4db8662e7b3d
Christopher Leggett <chris@leggett.dev>
2024-05-20 21:32:17 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -7,10 +7,25 @@ import (
 	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+type Request struct {
+    headerMap map[string]string
+    url string
+}
 
+func Parse(buffer []byte) (Request, error) {
+    reqLines := strings.Split(string(buffer), "\r\n")
+    headers := reqLines[1:len(reqLines)-2]
+    headerMap := make(map[string]string)
+    for _, header := range headers {
+        values := strings.Split(header, ": ")
+        headerMap[values[0]] = values[1]
+    }
+    reqLine := strings.Split(reqLines[0], " ")
+    urlPath := reqLine[1];
+    return Request{ headerMap: headerMap, url: urlPath }, nil
+}
+
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -27,16 +42,22 @@ func main() {
     if err != nil {
         fmt.Println("Error reading request")
     }
-    reqLines := strings.Split(string(buffer), "\r\n")
-    reqLine := strings.Split(reqLines[0], " ")
-    urlPath := reqLine[1];
+
+    request, _ := Parse(buffer)
 
     var response string
-    if (string(urlPath) == "/") {
+    if (request.url == "/") {
         response = "HTTP/1.1 200 OK\r\n\r\n"
-    } else if (strings.HasPrefix(urlPath, "/echo/")) {
-        echo := strings.TrimPrefix(urlPath, "/echo/")
+    } else if (strings.HasPrefix(request.url, "/echo/")) {
+        echo := strings.TrimPrefix(request.url, "/echo/")
         response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)
+    } else if (request.url == "/user-agent") {
+        userAgent := request.headerMap["User-Agent"]
+        if (userAgent == "") {
+            response = "HTTP/1.1 400 Bad Request\r\n\r\n"
+        } else {
+            response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+        }
     } else {
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     }
