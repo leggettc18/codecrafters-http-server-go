1b3b224b06107b770372116396a3bd3613fe1bd4
Christopher Leggett <chris@leggett.dev>
2024-05-20 20:00:26 -0400

    send a 200 response

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -16,9 +17,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+    conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+    conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+    
 }
