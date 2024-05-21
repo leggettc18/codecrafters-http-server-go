5d802fc7c68f20afbe2b455d74a5f20b5feff4c3
Christopher Leggett <chris@leggett.dev>
2024-05-20 21:51:30 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -12,33 +12,9 @@ type Request struct {
     url string
 }
 
-func Parse(buffer []byte) (Request, error) {
-    reqLines := strings.Split(string(buffer), "\r\n")
-    headers := reqLines[1:len(reqLines)-2]
-    headerMap := make(map[string]string)
-    for _, header := range headers {
-        values := strings.Split(header, ": ")
-        headerMap[values[0]] = values[1]
-    }
-    reqLine := strings.Split(reqLines[0], " ")
-    urlPath := reqLine[1];
-    return Request{ headerMap: headerMap, url: urlPath }, nil
-}
-
-func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-    conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleConnection(conn net.Conn) {
     buffer := make([]byte, 1024)
-    _, err = conn.Read(buffer)
+    _, err := conn.Read(buffer)
     if err != nil {
         fmt.Println("Error reading request")
     }
@@ -62,8 +38,34 @@ func main() {
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     }
     _, err = conn.Write([]byte(response))
-    if err != nil {
-        fmt.Println("Error writing data: ", err.Error())
-        os.Exit(1)
+}
+
+func Parse(buffer []byte) (Request, error) {
+    reqLines := strings.Split(string(buffer), "\r\n")
+    headers := reqLines[1:len(reqLines)-2]
+    headerMap := make(map[string]string)
+    for _, header := range headers {
+        values := strings.Split(header, ": ")
+        headerMap[values[0]] = values[1]
+    }
+    reqLine := strings.Split(reqLines[0], " ")
+    urlPath := reqLine[1];
+    return Request{ headerMap: headerMap, url: urlPath }, nil
+}
+
+func main() {
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+            os.Exit(1)
+        }
+        go handleConnection(conn)
     }
 }
